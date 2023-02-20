051dcf13d37f63aaebe4a168eada6ae9db097f35
unknown <Ho thi tai@PNVD223.pn.com.vn>
2023-02-20 07:38:51 +0700

    done

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -19,22 +19,106 @@
     
     </form>
   <?php
-   if(isset($_POST['btn'])){
-    $username = $_POST['username'];  
-    $password = $_POST['password']; 
-    $name=$_POST['tc'];
+  //  if(isset($_POST['btn'])){
+  //   $username = $_POST['username'];  
+  //   $password = $_POST['password']; 
+  //   $name=$_POST['tc'];
 
-    if ($username =='Ho Thi Tai' ){
+  //   if ($username =='Ho Thi Tai' ){
 
-      echo"<script> alert( 'password không tồn tại')</script>";
-    if($password=='5678') 
-      echo"<script> alert('Username không tồn tại') </script>";
-    }
-    else{
-      echo"<script> alert('Đăng nhập thành công') </script>";
-    }
-   }
+  //     echo"<script> alert( 'password không tồn tại')</script>";
+  //   if($password=='5678') 
+  //     echo"<script> alert('Username không tồn tại') </script>";
+  //   }
+  //   else{
+  //     echo"<script> alert('Đăng nhập thành công') </script>";
+  //   }
+  //  }
   ?>
   </div>
+</body>
+</html> -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chào Tài</title>
+</head>
+<body>
+    <?php
+    $login=array(
+        'students'=>array(
+            'SV1'=>array(
+            'mssv'=> 'V1',
+            'name' => 'Hồ Thị Tài',
+            'gender'=>'Nữ',
+            'hometown' => 'Quảng Trị',
+            'birthday'=>'2/3/2000',
+            'major' => 'IT'
+            ),
+            'SV2'=>array(
+            'mssv'=> 'V2',
+            'name' => 'Hồ Thị Tâm',
+            'gender'=>'Nữ',
+            'hometown' => 'Quảng Trị',
+            'birthday'=>'2/3/2000',
+            'major' => 'IT'
+            ),
+            'SV3'=>array(
+            'mssv'=> 'V3',
+            'name' => 'Hồ Thị Tươi',
+            'gender'=>'Nữ',
+            'hometown' => 'Quảng Trị',
+            'birthday'=>'17/5/1998',
+            'major' => 'IT'
+            ),
+            'SV4'=>array(
+                'mssv'=> 'V4',
+                'name' => 'Hồ Văn Tiếp',
+                'gender'=>'Nữ',
+                'hometown' => 'Quảng Trị',
+                'birthday'=>'30/4/1998',
+                'major' => 'IT'
+            )
+            
+            ),
+    )
+    ?>
+    <div id="content">
+        <?php
+        foreach($login as $key=>$value){
+        ?>
+        <table border=1 class="list_login">
+            <tr>
+                <th>ID</th>
+                <th>Ho Va Ten</th>
+                <th>Gioi tinh</th>
+                <th>Que quan</th>
+                <th>Ngay sinh</th>
+                <th>Nghề nghiệp</th>
+            </tr>
+            <?php foreach($value as $k=>$v){
+                ?>
+                <tr>
+                    <td><?php echo $k; ?></td>
+                    <td><?php echo $v['name']; ?></td>
+                    <td><?php echo $v['gender']; ?></td>
+                    <td><?php echo $v['hometown']; ?></td>
+                    <td><?php echo $v['birthday']; ?></td>
+                    <td><?php echo $v['major']; ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        <?php
+        }
+        ?> 
+    </div>
 </body>
 </html>
